55bb67a0b84d48f75cc11f8f6b887943df122d48
Takafumi Arakaki <aka.tkf@gmail.com>
2018-07-21 16:17:29 -0700

    Add some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,20 @@ else
     using Test
 end
 
-# write your own tests here
-@test 1 == 2
+macro test_nothrow(ex)
+    quote
+        @test begin
+            $(esc(ex))
+            true
+        end
+    end
+end
+
+@testset "Smoke tests" begin
+    @test_nothrow @ccode_warntype 1.0im + 1.0im
+    @test_nothrow @ccode_llvm     1.0im + 1.0im
+    @test_nothrow @ccode_native   1.0im + 1.0im
+    @test_nothrow @ccode_typed    1.0im + 1.0im
+    @test_nothrow @ccode_lowered  1.0im + 1.0im
+    @test_nothrow ColorfulCodeGen.highlight(@macroexpand @warn "hello")
+end
