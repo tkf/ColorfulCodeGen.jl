c21ad50f60440d04868311f1f59015d6f79582ac
Takafumi Arakaki <aka.tkf@gmail.com>
2018-07-21 18:10:58 -0700

    Use Compat.Test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,6 @@
 using ColorfulCodeGen
 using Compat: @warn
-@static if VERSION < v"0.7.0-DEV.2005"
-    using Base.Test
-else
-    using Test
-end
+using Compat.Test
 
 macro test_nothrow(ex)
     quote
